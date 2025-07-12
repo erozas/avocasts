b0a1413d9ee6314b824ac3905e5b6bf3efe81449
Exequiel Rozas <exequiel98@gmail.com>
2025-07-12 18:34:52 -0400

    Initial commit with user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  include Authentication
+  helper_method :user_signed_in?, :current_user
+
+  private
+  
+  def user_signed_in?
+    true
+  end
+
+  def current_user
+  end
 end
