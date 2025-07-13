f236d892db5e765a87f59f3708339ddc6a44f5a5
Exequiel Rozas <exequiel98@gmail.com>
2025-07-13 19:39:44 -0400

    Add flash notice

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,13 @@ class SessionsController < ApplicationController
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
 
   def new
+    @user = User.new
   end
 
   def create
-    if user = User.authenticate_by(params.permit(:email_address, :password))
+    if user = User.authenticate_by(params.expect(user: [:email_address, :password]))
       start_new_session_for user
-      redirect_to after_authentication_url
+      redirect_to after_authentication_url, notice: "You're logged in!"
     else
       redirect_to new_session_path, alert: "Try another email address or password."
     end
