1587daa21e1f4ffe4ae1c517c51cbd8020e75354
Exequiel Rozas <exequiel98@gmail.com>
2025-07-13 19:41:16 -0400

    Add 'set_referral_cookie' method and user helper methodS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,15 @@
 class ApplicationController < ActionController::Base
+  set_referral_cookie
   include Authentication
   helper_method :user_signed_in?, :current_user
 
   private
   
   def user_signed_in?
-    true
+    Current.user.present?
   end
 
   def current_user
+    Current.user
   end
 end
