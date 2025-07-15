8eca54aab6057f6d4c8f1cb3df906070f83f095a
Exequiel Rozas <exequiel98@gmail.com>
2025-07-14 22:53:22 -0400

    Remove 'HelloController'

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import CopyToClipboardController from "./copy_to_clipboard_controller"
+application.register("copy-to-clipboard", CopyToClipboardController)
