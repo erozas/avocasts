3bb5744affc531a410f2a5eb42ea06e5b617187a
Exequiel Rozas <exequiel98@gmail.com>
2025-07-23 00:53:17 -0400

    Callback to create a referral code for new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  after_create :create_referral_code
+
+  def referral_code
+    referral_codes.last.try(:code)
+  end
+
+  private
+
+  def create_referral_code
+    referral_codes.create
+  end
 end
