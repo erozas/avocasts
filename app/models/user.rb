4417feb5b94b76c3c187181a8b9697756e5a5ecb
Exequiel Rozas <exequiel98@gmail.com>
2025-08-03 22:47:34 -0400

    Qualifies for referral method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  REFERRAL_WINDOW = 30.days.freeze
+  MAX_ALLOWED_REFERRALS = 5.freeze
+
   include Refer::Model
   has_secure_password
   has_many :sessions, dependent: :destroy
@@ -9,6 +12,22 @@ class User < ApplicationRecord
 
   def referral_code
     referral_codes.last.try(:code)
+  end
+
+  def referral_code_id
+    referral_codes.last.try(:id)
+  end
+
+  def qualifies_for_referral?
+    created_at >= REFERRAL_WINDOW.ago && referrer.referrals.count < MAX_ALLOWED_REFERRALS
+  end
+
+  def subscription_status
+    ["Active", "Trial"].sample
+  end
+
+  def subscription_plan
+    ["Pro", "None"].sample
   end
 
   private
