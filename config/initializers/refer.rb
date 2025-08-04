2e1d262e270243dac1a482ed7d283029f4c349aa
Exequiel Rozas <exequiel98@gmail.com>
2025-08-03 22:46:50 -0400

    Custom code generator with 'SecureRandom.alphanumeric'

--- config/initializers/refer.rb
+++ config/initializers/refer.rb
@@ -1,2 +1,2 @@
-Refer.code_generator = -> (referrer) { "#{referrer.id}-#{SecureRandom.hex(6)}".upcase }
+Refer.code_generator = -> (referrer) { "#{referrer.id}-#{SecureRandom.alphanumeric(6)}".upcase }
 Refer.cookie_length = 15.days
